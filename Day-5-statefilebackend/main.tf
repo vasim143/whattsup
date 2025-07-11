32c768770e6a9362ee6d6dfd61ad627dc3d65923
veera <veerababu.narni232@gmail.com>
2025-07-11 04:51:02 +0000

    updated

--- Day-5-statefilebackend/main.tf
+++ Day-5-statefilebackend/main.tf
@@ -1,14 +1,9 @@
 resource "aws_instance" "name" {
     ami = "ami-05ffe3c48a9991133"
     instance_type = "t2.micro"
+    tags = {
+      Name = "state"
+    }
     
-  
-}
-resource "aws_vpc" "name" {
-    cidr_block = "10.0.0.0/16"
-  
-}
-resource "aws_vpc" "name2" {
-    cidr_block = "10.0.0.0/24"
   
 }
